926b225859f2cdda2b2f67ed8a52bb4de7b4d06d
calebeallen <calebeallen318@gmail.com>
2025-10-08 18:47:36 -0400

    db setup

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,31 @@
 import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
 
-const app = express();
-app.get("/", (_req, res) => res.send("Hello Worldd!"));
+dotenv.config();
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const uri = "mongodb+srv://root:" + process.env.MONGO_PWD + "@be4realdb.o2hgd3j.mongodb.net/?retryWrites=true&w=majority&appName=Be4RealDB";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+try {
+    await client.connect();
+    await client.db("Be4RealDB").command({ ping: 1 });
+
+    const app = express();
+    app.get("/", (_req, res) => res.send("Hello Worldd!"));
+
+    app.listen(3000, () => console.log("Server running on port 3000"));
+
+} catch (e) {
+    console.error("Server crashed" + e);
+} finally {
+    await client.close();
+}
