d653c1cb3bc9a1b8f16c235b00daa87ebe9ef9cd
dvcalex <a.cruz@limbitless-solutions.org>
2025-11-16 19:02:01 -0500

    get_single_post test

--- api/src/endpoints/post/get/get_single_post.ts
+++ api/src/endpoints/post/get/get_single_post.ts
@@ -2,16 +2,17 @@ import {Request, Response} from "express";
 import {getDB} from "../../../utils/mongo.js";
 import {DBPost} from "../../../types/mongo_schemas.js";
 //import {getJWT, checkJWT} from "../../utils/jwt.js";
-import {ObjectId} from "mongodb";
+import {ObjectId, Db} from "mongodb";
 
 /**
  * Gets a single post by ID
  *
  * @param req.params - post id passed in through route param
+ * @param db - Optional database instance (for testing)
  *
  * (ex. http://<IP>:<PORT>/api/post/<UNIQUE_MONGO_ObjectId_FOR_POST>)
  */
-export default async function getSinglePost(req: Request, res: Response) {
+export default async function getSinglePost(req: Request, res: Response, db?: Db) {
     try {
         // don't need jwt auth if posts are visible to anyone
         // const {err: getJWTErr, token} = getJWT(req.headers.authorization);
@@ -46,8 +47,9 @@ export default async function getSinglePost(req: Request, res: Response) {
             });
         }
 
-        const db = await getDB();
-        const postsColl = db.collection<DBPost>("posts");
+        // Use injected db or get from connection
+        const database = db || await getDB();
+        const postsColl = database.collection<DBPost>("posts");
 
         // find the post
         const post = await postsColl.findOne({_id: new ObjectId(postId)});
